33f7505c550deffad406559d0d230df7479a8efd
Braden M. Kelley <redbmk@gmail.com>
2018-07-04 19:12:53 -0700

    add REST server

---
+++ src/server/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config({ path: '.env.local' });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue');
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) throw new Error('Authorization header is required');
+
+    const accessToken = req.headers.authorization.trim().split(' ')[1];
+    await oktaJwtVerifier.verifyAccessToken(accessToken);
+    next();
+  } catch (error) {
+    next(error.message);
+  }
+});
+
+const database = new Sequelize({
+  dialect: 'sqlite',
+  storage: './test.sqlite',
+});
+
+const Post = database.define('posts', {
+  title: Sequelize.STRING,
+  body: Sequelize.TEXT,
+});
+
+epilogue.initialize({ app, sequelize: database });
+
+epilogue.resource({
+  model: Post,
+  endpoints: ['/posts', '/posts/:id'],
+});
+
+const port = process.env.SERVER_PORT || 3001;
+
+database.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+});
